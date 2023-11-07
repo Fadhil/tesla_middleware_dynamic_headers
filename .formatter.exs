0aab8542c562f28738300f1a0c86afdf4d4225a0
Jake Morrison <jake@cogini.com>
2023-11-07 17:33:33 -0600

    Disable Styler for now

--- .formatter.exs
+++ .formatter.exs
@@ -1,4 +1,5 @@
 [
-  plugins: [Styler],
+  # Styler doesn't work on older versions of Elixir, so comment out for now
+  # plugins: [Styler],
   inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"]
 ]
